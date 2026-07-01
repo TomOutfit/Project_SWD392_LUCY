6089a11a733ca452a769253350f7ac6edfc4ecb9
TomOutfit <TomOutfit2020@gmail.com>
2026-07-01 21:30:37 +0700

    fix: CORS - clean SetIsOriginAllowed + nginx OPTIONS preflight handler

--- packages/net-service/Program.cs
+++ packages/net-service/Program.cs
@@ -52,11 +52,11 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddCors(opt =>
     opt.AddDefaultPolicy(policy =>
-        policy.WithOrigins("http://localhost:5173", "https://lucygroup.vercel.app", "http://lucygroup.vercel.app")
-              .SetIsOriginAllowed(_ => true)
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials()));
+        policy
+            .SetIsOriginAllowed(_ => true)   // Echo any Origin back (needed for credentials)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()));
 
 var app = builder.Build();
 
